4774a178fcaaf396eecfd7a9b46bde34a40fa7e9
Anythingfordev <anythingfordevelopment@gmail.com>
2025-07-25 11:54:10 +0530

    Add useEffect import to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
 import { Mountain, Route } from 'lucide-react'
 import { Header } from './components/Header'
